4b0830f9c928744a2c91eb525185dc78b53e2cb9
Vadim Shalimov <daishi007@mail.ru>
2020-03-24 19:34:12 +0000

    remake .sh files

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
-#!/bin/bash 
-git clone -b monolith https://github.com/express42/reddit.git\ncd reddit && bundle install\npuma -d
+#!/bin/bash
+git clone -b monolith https://github.com/express42/reddit.git
+cd reddit && bundle install
+puma -d
--- startup_config.sh
+++ startup_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-#install ruby
+# install ruby
 sudo apt install -y ruby-full ruby-bundler build-essential
 # install mongodb
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xd68fa50fea312927
