3da1876636f5fde5e4fef2ac1006216213600da7
Vdaishi <61919404+Vdaishi@users.noreply.github.com>
2020-03-24 20:35:15 +0300

    Added ip address and startup_config.sh

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Vdaishi_infra
 Vdaishi Infra repository
+gcloud compute instances create reddit-app-auto --boot-disk-size=10GB --image-family ubuntu-1604-lts --image-project=ubuntu-os-cloud --machine-type=g1-small --tags puma-server --restart-on-failure --metadata-from-file startup-script=./startup_config.sh --zone=europe-west4-a
+testapp_IP = 34.90.229.174
+testapp_port = 9292
---
+++ startup_config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo apt update
+#install ruby
+sudo apt install -y ruby-full ruby-bundler build-essential
+# install mongodb
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xd68fa50fea312927
+sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
+sudo apt update
+sudo apt install -y mongodb-org
+#deploy app
+git clone -b monolith https://github.com/express42/reddit.git
+cd reddit && bundle install
+puma -d
