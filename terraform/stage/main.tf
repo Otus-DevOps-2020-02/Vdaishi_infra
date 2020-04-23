367315a0b6a8e28dbe96a5c86cb27656f68d2a5f
Vadim Shalimov <daishi007@mail.ru>
2020-04-23 19:32:46 +0000

    added at stage private key variables

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -22,6 +22,8 @@ module "app" {
   public_key_path = var.public_key_path
   zone            = var.zone
   app_disk_image  = var.app_disk_image
+  private_key_path = var.private_key_path
+  database_url     = module.db.db_internal_ip
 }
 
 module "db" {
@@ -29,6 +31,7 @@ module "db" {
   public_key_path = var.public_key_path
   zone            = var.zone
   db_disk_image   = var.db_disk_image
+  private_key_path = var.private_key_path
 }
 module "vpc" {
   source        = "../modules/vpc"
